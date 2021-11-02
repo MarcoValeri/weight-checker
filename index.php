3aec2bba2bb9dc3ed8672993f9a2a3bdda941d0b
Marco Valeri <marcovaleri@hotmail.it>
2021-11-02 08:41:36 +0000

    Create homepage

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0.">
+        <title>Weight Checker</title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="./index.php">Home</a></li>
+                    <li><a href="#">Register</a></li>
+                </ul>
+            </nav>
+            <section>
+                <h1>Weight Checker</h1>
+            </section>
+        </header>
+    </body>
+</html>
